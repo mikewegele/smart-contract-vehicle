80e9ea83db5f1e67054d20425f31c56a1dbddd1f
mikewegele <mike.wegele@icloud.com>
2025-05-14 19:27:16 +0200

    add api

--- frontend/src/layout/App.tsx
+++ frontend/src/layout/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 import LoginScreen from "../screens/LoginScreen.tsx";
 import DashboardScreen from "../screens/DashboardScreen.tsx";
@@ -6,15 +6,30 @@ import DashboardScreen from "../screens/DashboardScreen.tsx";
 
 const App: React.FC = () => {
 
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:5147/api/hello')
+        .then((res) => res.json())
+        .then((data) => setMessage(data.message))
+        .catch((err) => console.error('API Error:', err));
+  }, []);
+
   return (
-      <Router>
-        <Routes>
-          <Route path="/login" element={<LoginScreen />} />
-          <Route
-              path="/dashboard" element={<DashboardScreen />}/>
-          <Route path="*" element={<Navigate to="/login" replace />} />
-        </Routes>
-      </Router>
+      <>
+          <Router>
+              <Routes>
+                  <Route path="/login" element={<LoginScreen />} />
+                  <Route
+                      path="/dashboard" element={<DashboardScreen />}/>
+                  <Route path="*" element={<Navigate to="/login" replace />} />
+              </Routes>
+          </Router>
+          <div>
+              <h1>Nachricht vom Backend:</h1>
+              <p>{message}</p>
+          </div>
+      </>
   );
 }
 
