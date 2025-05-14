24c49a13c0533d417060dbe62b4ca70b41649f67
mikewegele <mike.wegele@icloud.com>
2025-05-14 19:28:20 +0200

    remove login

--- frontend/src/layout/App.tsx
+++ frontend/src/layout/App.tsx
@@ -1,8 +1,4 @@
 import React, {useEffect, useState} from 'react';
-import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
-import LoginScreen from "../screens/LoginScreen.tsx";
-import DashboardScreen from "../screens/DashboardScreen.tsx";
-
 
 const App: React.FC = () => {
 
@@ -16,20 +12,10 @@ const App: React.FC = () => {
   }, []);
 
   return (
-      <>
-          <Router>
-              <Routes>
-                  <Route path="/login" element={<LoginScreen />} />
-                  <Route
-                      path="/dashboard" element={<DashboardScreen />}/>
-                  <Route path="*" element={<Navigate to="/login" replace />} />
-              </Routes>
-          </Router>
           <div>
               <h1>Nachricht vom Backend:</h1>
               <p>{message}</p>
           </div>
-      </>
   );
 }
 
