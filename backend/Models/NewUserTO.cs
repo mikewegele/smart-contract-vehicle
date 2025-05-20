485a6fa34fb744dab7a554b936231fcaf05efc85
voic00 <voic00@mi.fu-berlin.de>
2025-05-20 11:30:11 +0200

    typo fix in namespace declaration

--- backend/Models/NewUserTO.cs
+++ backend/Models/NewUserTO.cs
@@ -1,7 +1,8 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace SmartContractVehicle.Models
+namespace SmartContractVehicle.Models;
+
 public class NewUserTO
 {
     [Required, EmailAddress]
