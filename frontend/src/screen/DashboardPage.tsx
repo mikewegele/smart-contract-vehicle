ac837134c2d5ca26186637cf0e8e532fbf7a4311
mikewegele <mike.wegele@icloud.com>
2025-05-21 10:28:54 +0200

    add map

--- frontend/src/screen/DashboardPage.tsx
+++ frontend/src/screen/DashboardPage.tsx
@@ -3,13 +3,14 @@ import Container from "../components/container/Container.tsx";
 import { useNavigate } from "react-router-dom";
 import SettingsIcon from '@mui/icons-material/Settings';
 import DefaultButton from "../components/button/DefaultButton.tsx";
+import SimpleMap from "../components/map/VehicleMap.tsx";
 
 const DashboardPage: React.FC = () => {
     const navigate = useNavigate();
 
     return (
         <Container>
-            <VehicleMap/>
+            <SimpleMap/>
             <DefaultButton
                 variant="contained"
                 color="primary"
