48e4127350a074e6aaedab3ace5c43254fe0725c
voic00 <voic00@mi.fu-berlin.de>
2025-05-27 22:46:50 +0200

    add dummy query to CarController

---
+++ backend/Controller/CarController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartContractVehicle.Data;
+using SmartContractVehicle.DTO;
+using NetTopologySuite.Geometries;
+using Microsoft.OpenApi.Extensions;
+
+namespace SmartContractVehicle.Controller
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CarController(AppDbContext db) : ControllerBase
+    {
+        private readonly AppDbContext _db = db;
+
+        [HttpGet]
+        public ActionResult<IQueryable<CarTO>> Query()
+        {
+            IList<CarTO> cars = [
+                new() {
+                    Trim = new VehicleTrimTO
+                    {
+                        Model = new VehicleModelTO
+                        {
+                            Producer = new AutomotiveCompanyTO
+                            {
+                                Name = "BMW",
+                            },
+                            Name = "iX",
+                        },
+                        Name = "M60",
+                        Fuel = FuelType.Electric.GetDisplayName(),
+                        Drivetrain = Drivetrain.AllWheelDrive.GetDisplayName(),
+                    },
+                    CurrentPosition = new Point(new Coordinate(11.576124, 48.137154)),
+                    RemainingReach = 5555.5,
+                    Colour = "black",
+                }
+            ];
+            
+            return Ok(cars);
+        }
+
+    }
+}
