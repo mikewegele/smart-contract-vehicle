87da17d74b11ce932ff094379b275c6d65d961ec
voic00 <voic00@mi.fu-berlin.de>
2025-05-27 21:15:53 +0200

    add remaining reach for cars

--- backend/Model/Car.cs
+++ backend/Model/Car.cs
@@ -19,6 +19,8 @@ namespace SmartContractVehicle.Model
 
         [Column(TypeName = "geography")]
         public required Point CurrentPosition { get; set; }
+
+        public double RemainingReach { get; set; }
     }
 }
 
