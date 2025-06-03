cf9e416ed4f4d47c873a198322800918e8bcaf68
voic00 <voic00@mi.fu-berlin.de>
2025-06-03 18:22:11 +0200

    fix: typo

--- backend/Data/AppDbContext.cs
+++ backend/Data/AppDbContext.cs
@@ -7,7 +7,7 @@ namespace SmartContractVehicle.Data;
 public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<User>(options)
 {
     public DbSet<Address> Addresses { get; set; }
-    public DbSet<AutomotiveCompany> AutomotiveCompanys { get; set; }
+    public DbSet<AutomotiveCompany> AutomotiveCompanies { get; set; }
     public DbSet<VehicleModel> VehicleModels { get; set; }
     public DbSet<VehicleTrim> VehicleTrims { get; set; }
     public DbSet<Car> Cars { get; set; }
