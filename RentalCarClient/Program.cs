b6610afb1ae743600830ac5a51bd3c6c54a4e935
voic00 <voic00@mi.fu-berlin.de>
2025-06-15 09:31:00 +0200

    upd-2

--- RentalCarClient/Program.cs
+++ RentalCarClient/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using var client = new HttpClient();
+
+try
+{
+    var url = "https://contoso.com/";
+    Console.WriteLine($"Sende GET-Anfrage an {url} ...");
+
+    var response = await client.GetAsync(url);
+    response.EnsureSuccessStatusCode();
+
+    var content = await response.Content.ReadAsStringAsync();
+    Console.WriteLine("Antwort erhalten:");
+    Console.WriteLine(content[..Math.Min(500, content.Length)] + "...");
+}
+catch (HttpRequestException e)
+{
+    Console.WriteLine($"Fehler bei der Anfrage: {e.Message}");
+}
