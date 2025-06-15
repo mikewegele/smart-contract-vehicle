81064133419eceb899e58da4dadff1943f4a7742
voic00 <voic00@mi.fu-berlin.de>
2025-06-15 12:32:36 +0200

    wip

---
+++ backend/DTO/CarState.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartContractVehicle.DTO
+{
+    public class CarState
+    {
+        public string DeviceId { get; set; }
+        public string Status { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
---
+++ backend/Service/Interfaces/ICarStateSerivce.cs
@@ -0,0 +1,11 @@
+﻿using SmartContractVehicle.DTO;
+
+namespace SmartContractVehicle.Service
+{
+    public interface ICarStateService
+    {
+        Task<CarState?> GetAsync(string deviceId);
+        Task UpdateAsync(CarState state);
+    }
+
+}
