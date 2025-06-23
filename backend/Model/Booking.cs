7d2b33239527c48cfc94ea15e9ff90df0fe1c01e
Eyal Yehuda <eyal.yehuda11@gmail.com>
2025-06-23 12:23:40 +0200

    Updated EndRide

--- backend/Model/Booking.cs
+++ backend/Model/Booking.cs
@@ -1,5 +1,6 @@
 using SmartContractVehicle.Data;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.InteropServices;
 
 namespace SmartContractVehicle.Model;
 
@@ -18,21 +19,31 @@ public class Booking
     public DateTime StartTimeUTC { get; private set; } = DateTime.UtcNow;
     public DateTime? EndTimeUTC { get; private set; }
 
+    // public Location StartLoc { get; private set; }   
+    // public Location EndLoc { get; private set; }   
+
     public bool IsCompleted { get; private set; } = false;
 
-    public double? TotalDistance { get; private set; }
+    public decimal PricePerMinute { get; private set; }
+    public decimal? TotalTime { get; private set; }
+
+    public decimal? TotalDistance { get; private set; }
     public decimal? TotalCost { get; private set; }
+
 
     public Booking() { }
     public Booking(string rentor)
     {
         RentorId = rentor;
     }
-    public void EndRide(double distanceKm, decimal cost)
+    public void EndRide(decimal distanceKm, decimal totaltime)
     {
+        if (IsCompleted)
+            throw new InvalidOperationException("Ride already completed.");
         IsCompleted = true;
         EndTimeUTC = DateTime.UtcNow;
         TotalDistance = distanceKm;
-        TotalCost = cost;
+        TotalTime = totaltime;
+        TotalCost = totaltime * PricePerMinute;
     }
 }
